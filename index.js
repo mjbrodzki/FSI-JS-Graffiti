c1817730b356d4aa51999f722675d4fbd4fbb75f
Marysia Brodzki <mjbrodzki@gmail.com>
2022-07-21 20:23:00 -0700

    swe class activty changes

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+
+header.style.textAlign = "left"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let footer = document.querySelector(".footer")
+footer.style.color= 'red'
+
+
+
+
+
+
+
